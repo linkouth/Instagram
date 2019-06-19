42895c618fe53399992899ea4f5ceb361ee3864c
Timur <dkatimullin@gmail.com>
2019-06-19 19:25:49 +0400

    Done Weekday finder

--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -16,7 +16,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var resultLabel: UILabel!
     
     @IBAction func findDay() {
+        var dateComponets = DateComponents()
+        guard let day = dateTF.text, let month = monthTF.text, let year = yearTF.text else { return }
+        dateComponets.day = Int(day)
+        dateComponets.month = Int(month)
+        dateComponets.year = Int(year)
         
+        let calendar = Calendar.current
+        guard let date = calendar.date(from: dateComponets) else { return }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        
+        let weekday = dateFormatter.string(from: date)
+        
+        resultLabel.text = weekday
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
